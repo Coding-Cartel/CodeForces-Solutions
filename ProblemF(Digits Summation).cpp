bd2589cbe2705cc777c6bd1c0a765f1b48e4c799
Aditi Chhabra <121351859+AditiChhabra@users.noreply.github.com>
2023-10-26 17:10:57 +0530

    Added New Submission.

---
+++ ProblemF(Digits Summation).cpp
@@ -0,0 +1,17 @@
+// codeforces problem F in cpp
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   int a,b;
+   cin>>a>>b;
+   
+       int var1=a%10;
+       int var2=b%10;
+       
+       int sum= var1+var2;
+       cout<<sum<<endl;
+       return 0;
+}
+   
